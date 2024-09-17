ff0c5b11cd567f3305f6e65076666a7f82fa892a
Lautaro Pavez <lautapavez@gmail.com>
2024-09-16 23:35:40 -0300

    Cambios realizados

--- ex_9_list_comprehensions.py
+++ ex_9_list_comprehensions.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 # forma 1
     for i in range(x + 1):
         for j in range(y + 1):
-            for k in range(y + 1):
+            for k in range(z + 1):
                 if i+j+k != n:
                     print(i, j, k)
 
